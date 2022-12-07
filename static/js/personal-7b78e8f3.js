f835f7c534840ac8b181de8509151a90b820d7ff
duidui <88029142+duiduiku@users.noreply.github.com>
2022-12-07 18:59:23 +0800

    Delete static directory

--- static/js/personal-7b78e8f3.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as n}from"./index-b8671837.js";const o={};function r(t,s){return c(),n("div",null," \u4E2A\u4EBA\u7EDF\u8BA1 ")}const _=e(o,[["render",r]]);export{_ as default};
--- static/js/tou-065b6f06.js
+++
@@ -1,1 +0,0 @@
-const e=""+new URL("../webp/tou-bac63f8a.webp",import.meta.url).href;export{e as _};
