7a0f0ac01186440747d7591791f5bb42ed90c313
duidui <88029142+duiduiku@users.noreply.github.com>
2022-12-07 18:39:56 +0800

    Add files via upload
    yes

---
+++ static/js/private-1a501b35.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as r}from"./index-b8671837.js";const t={};function n(o,_){return c(),r("div",null," \u79C1\u4EBA\u9886\u57DF ")}const s=e(t,[["render",n]]);export{s as default};
--- static/js/tou-065b6f06.js
+++ static/js/tou-065b6f06.js
@@ -1,1 +1,1 @@
-const t="/static/webp/tou-bac63f8a.webp";export{t as _};
+const e=""+new URL("../webp/tou-bac63f8a.webp",import.meta.url).href;export{e as _};
